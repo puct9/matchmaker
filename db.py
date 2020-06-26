60c43be56f8de2ee3fe39f6eab64ac8546a2023b
Luke Yang <lukeybsn@gmail.com>
2020-06-27 00:34:43 +1000

    db api update

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import random
 import string
+from copy import deepcopy
 
 CHARSET = string.ascii_uppercase + string.digits
 
@@ -18,8 +19,20 @@ class MMDB:
         # returns {player: response, ...}, mode
         pass
 
+    def room_exists(self, room_id):
+        pass
+
     def set_response(self, response_id, prefs):
         # the response_id is mapped to a room
+        pass
+
+    def response_exists(self, response_id):
+        pass
+
+    def response_id_to_room(self, response_id):
+        pass
+
+    def response_id_to_player(self, response_id):
         pass
 
 
@@ -37,6 +50,7 @@ class SimpleDB(MMDB):
             room_id = ''.join(random.choices(CHARSET, k=6))
         self.rooms[room_id] = {
             'mode': 'friend',
+            'players': [p for p in players],
             'player_info': {p: None for p in players},
             'response_ids': {}
         }
@@ -51,7 +65,10 @@ class SimpleDB(MMDB):
         return room_id
 
     def get_room_info(self, room_id):
-        return self.rooms[room_id]
+        return deepcopy(self.rooms.get(room_id))
+
+    def room_exists(self, room_id):
+        return self.rooms.get(room_id) is not None
 
     def set_response(self, response_id, prefs):
         room_id = self.reverse_mapping.get(response_id)
@@ -59,3 +76,14 @@ class SimpleDB(MMDB):
             return
         player = self.rooms[room_id]['response_ids'][response_id]
         self.rooms[room_id]['player_info'][player] = prefs
+
+    def response_exists(self, response_id):
+        return self.reverse_mapping.get(response_id) is not None
+
+    def response_id_to_room(self, response_id):
+        return self.reverse_mapping.get(response_id)
+
+    def response_id_to_player(self, response_id):
+        if self.response_exists(response_id):
+            return self.rooms[self.reverse_mapping[response_id]
+                              ]['response_ids'][response_id]
