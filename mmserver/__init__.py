c8a1c3e9e4d0feb0b97ab29abaca5438b5784eb5
Luke Yang <lukeybsn@gmail.com>
2020-07-26 19:52:23 +1000

    Update endpoints

--- mmserver/__init__.py
+++ mmserver/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint, redirect, url_for
 
 from .apps import create_app, socketio
 from .apps.mmv1 import app as mmv1_app
+from .apps.moneydraft import app as draftv1_app
 
 app = create_app(debug=True)
 app.template_folder = '../templates'
@@ -12,6 +13,7 @@ app.register_blueprint(main_bp)
 
 # other blueprints
 app.register_blueprint(mmv1_app, url_prefix='/mmv1')
+app.register_blueprint(draftv1_app, url_prefix='/draftv1')
 
 
 @app.route('/')
