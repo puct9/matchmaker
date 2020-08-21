83a0138851f72d7a7b933af770af1a14f497a51a
Luke Yang <lukeybsn@gmail.com>
2020-08-21 14:05:53 +1000

    Home page optimisation

--- mmserver/apps/__init__.py
+++ mmserver/apps/__init__.py
@@ -9,7 +9,8 @@ socketio = SocketIO()
 
 FAVICON_URL = 'http://mmvm-cdn.storage.googleapis.com/favicon.ico'
 BONUS_DICT = {
-    'logo_url': 'http://mmvm-cdn.storage.googleapis.com/meme-transparent.png',
+    'logo_url': 'http://mmvm-cdn.storage.googleapis.com/'
+                'meme-transparent-reduced.png',
     'navbar_icon_url': 'http://mmvm-cdn.storage.googleapis.com/lrr.png',
     'favicon_url': FAVICON_URL,
     'source_link': 'https://github.com/thejhonnyguy/matchmaker',
